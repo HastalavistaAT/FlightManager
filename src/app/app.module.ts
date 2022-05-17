18fe44cfa53b09c0fb32f7a71c2751e407ee03a5
HastalavistaAT <gerd@gerd.at>
2022-05-17 14:19:57 +0200

    will add flight components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { FlightModule } from './flight/flight.module';
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+import { FLIGHT_ROUTES } from "./flight/flight.routes";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, 
+    HttpClientModule,
+    RouterModule.forRoot([...FLIGHT_ROUTES]), 
+    FlightModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
